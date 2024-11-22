6a95c81c1ef3894d97248d6e30fe6d58941d4843
Alex Ramirez Git <alexramirezzz2004@gmail.com>
2024-11-22 19:49:00 -0300

    parcial2023 py hecho

--- Python/Parciales/parcial2023.py
+++ Python/Parciales/parcial2023.py
@@ -89,3 +89,35 @@ def posiciones_carreras (carreritas: dict[str,list[str]], nombre_esp:str, e:int)
 #carre= {"carrera1":["linda", "petisa", "mister", "luck"], "carrera2":["petisa", "mister", "linda", "luck"], "carrera3":["petisa", "mister", "linda", "luck"]}
 
 #print (frecuencia_posiciones_por_caballo (c, carre))
+
+#4) Matriz capicúa [3 puntos]
+#Implementar la función matriz_capicua que dada una matriz devuelve True si 
+#cada una de sus filas es capicúa. Es decir, si cada fila es igual leída de 
+#izquierda a derecha o de derecha a izquierda. Definimos a una secuencia de
+#secuencias como matriz si todos los elemento de la primera secuencia tienen 
+#la misma longitud.
+
+#problema matriz_capicua(in m:seq⟨seq⟨Z⟩⟩ ) : Bool {
+#  requiere: {todos los elementos de m tienen igual longitud (los elementos de m son secuencias)}
+#  asegura: {(res = true) <=> todos los elementos de m son capicúa}
+#}
+
+#m = [[1,2,2,1],[-5,6,6,-5],[0,1,1,0]] #-> true
+def matriz_capicua (m:list[list[int]]) -> bool:
+    for seq in m: #[1,2,2,1]
+        if verificar_capicua (seq) == False:
+            return False
+        
+    return True
+
+def verificar_capicua (s: list[int]) -> bool:
+    lista_alrevez: list[int]= []
+
+    for i in range (len(s)-1,-1,-1):
+        lista_alrevez.append(s[i])
+
+    return s == lista_alrevez
+
+#m = [[1,2,2],[6,-5,6],[0,1,1]]
+
+#print (matriz_capicua (m))
